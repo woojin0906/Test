794c7bddfec7deef7502378b703938133a68ab69
jwjle <jwjle@DESKTOP-G53Q3DO>
2022-03-03 19:24:54 +0900

    Test

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("¾È´¨, ÀÚ¹Ù!!");
+		System.out.println(123);
+	}
+
+}
